d430b968755d673cae38ec7cfc7dc971d8b8ad92
mauroche <rochemaui165@gmail.com>
2024-05-02 18:54:38 +0800

    add pause time function

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
 let startButton = document.querySelector('#start_button');
+let pauseButton = document.querySelector('#pause_button');
 let displayCount = document.querySelector('#count');
 let count = 0;
 let timer;
@@ -7,9 +8,14 @@ function startTime() {
     timer = setInterval(updateCount, 1000);
 }
 
+function pauseTime() {
+    clearInterval(timer);
+}
+
 function updateCount() {
     count++;
     displayCount.innerText = count;
 }
 
 startButton.addEventListener('click', startTime);
+pauseButton.addEventListener('click', pauseTime);
